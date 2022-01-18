eecb0917f1ba119a2d8d893a95bd5c10931c9158
sahinyoldas <sahinyoldas@gmail.com>
2022-01-18 01:04:42 +0000

    ** musicalog.webapi added

---
+++ Musicalog.WebAPI/Controllers/AlbumsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Musicalog.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumsController(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {       
+            var result = await _albumService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return  BadRequest(result);
+        }
+    }
+}
