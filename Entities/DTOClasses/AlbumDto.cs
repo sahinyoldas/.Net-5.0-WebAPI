90af5fab41b670df42b345d06cd01b301c524d0f
sahinyoldas <sahinyoldas@gmail.com>
2022-03-10 00:25:02 +0000

    ** albumdto added

---
+++ Entities/DTOClasses/AlbumDto.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOClasses
+{
+    public class AlbumDto : IBaseDto
+    {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Year { get; set; }
+        public long Stock { get; set; }
+        public AlbumType AlbumType { get; set; }
+    }
+}
